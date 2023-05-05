f7257eb54d4245edd1c81716ed305cc1357fc05b
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-05-05 17:26:29 +0200

    endsWith() method

--- src/main/java/com/example/exercismjavakatas/Arrays/SmileFaces.java
+++ src/main/java/com/example/exercismjavakatas/Arrays/SmileFaces.java
@@ -7,5 +7,12 @@ public class SmileFaces {
     public static int countSmileys(List<String> arr) {
         return (int) arr.stream().filter(s -> s.matches("^[: ;][-~]?[)D]")).count();
     }
+
+    public static boolean solution(String str, String ending) {
+        if(ending.length()>str.length()) return false;
+        int beginning = str.length() - ending.length();;
+        String splitted = str.substring(beginning);
+        return splitted.equals(ending);
+    }
 }
 
