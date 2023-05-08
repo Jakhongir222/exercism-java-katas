f1d44a61c0aeca614396b6a579d4d0c93fd9cdb0
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-05-08 22:37:53 +0200

    V

--- src/main/java/com/example/exercismjavakatas/CountDuplicates/Persist.java
+++ src/main/java/com/example/exercismjavakatas/CountDuplicates/Persist.java
@@ -1,5 +1,8 @@
 package com.example.exercismjavakatas.CountDuplicates;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Persist {
     public static int persistence(int num) {
         int count = 0;
@@ -14,5 +17,18 @@ public class Persist {
             count++;
         }
         return count;
+    }
+
+    public static Map<Character, Integer> count(String str) {
+        Map<Character , Integer> map = new HashMap<>();
+        if(str == null) {
+            return map;
+        }
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1); // increment count for character, Map.getOrDefault
+            // it will get the correct incremented number or by default O and adds 1
+        }
+        return map;
     }
 }
--- src/test/java/com/example/exercismjavakatas/CountDuplicates/PersistTest.java
+++ src/test/java/com/example/exercismjavakatas/CountDuplicates/PersistTest.java
@@ -1,6 +1,10 @@
 package com.example.exercismjavakatas.CountDuplicates;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,5 +16,58 @@ public class PersistTest {
         assertEquals(0, Persist.persistence(  4), "Incorrect answer for n=4"  );
         assertEquals(2, Persist.persistence( 25), "Incorrect answer for n=25" );
         assertEquals(4, Persist.persistence(999), "Incorrect answer for n=999");
+    }
+
+    @Test
+    @Order(1)
+    public void testEmptyString() {
+        Map<Character, Integer> d = new HashMap<>();
+        assertEquals(d, Persist.count(""));
+    }
+
+    @Test
+    @Order(2)
+    public void testSingleCharacter() {
+        Map<Character, Integer> d = new HashMap<>();
+        d.put('a', 1);
+        assertEquals(d, Persist.count("a"));
+    }
+
+    @Test
+    @Order(3)
+    public void testMultipleCharacters() {
+        Map<Character, Integer> d = new HashMap<>();
+        d.put('a', 3);
+        d.put('b', 3);
+        d.put('c', 1);
+        assertEquals(d, Persist.count("aabbbac"));
+    }
+
+    @Test
+    @Order(4)
+    public void testAllCharactersUnique() {
+        Map<Character, Integer> d = new HashMap<>();
+        d.put('a', 1);
+        d.put('b', 1);
+        d.put('c', 1);
+        assertEquals(d, Persist.count("abc"));
+    }
+
+    @Test
+    @Order(5)
+    public void testAllCharactersSame() {
+        Map<Character, Integer> d = new HashMap<>();
+        d.put('a', 5);
+        assertEquals(d,  Persist.count("aaaaa"));
+    }
+
+    @Test
+    @Order(6)
+    public void testDifferentCharacters() {
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('a', 2);
+        expected.put('b', 2);
+        Map<Character, Integer> actual = Persist.count("aabb");
+        assertEquals(expected, actual);
     }
 }
