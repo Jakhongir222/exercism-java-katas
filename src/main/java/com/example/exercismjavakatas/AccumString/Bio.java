743dff7538e651017dc2fab903789f82d269fdca
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-05-18 16:28:29 +0200

    I have to create helper methods to keep my code cleaner

--- src/main/java/com/example/exercismjavakatas/AccumString/Bio.java
+++ src/main/java/com/example/exercismjavakatas/AccumString/Bio.java
@@ -1,8 +1,51 @@
 package com.example.exercismjavakatas.AccumString;
 
 public class Bio {
+
     public String dnaToRna(String dna){
         String result = dna.replaceAll("T", "U");
         return result;
     }
+
+
+
+    public static String revRot(String str, int sz) {
+        StringBuilder sb = new StringBuilder();
+        if (sz <= 0 || str.isEmpty() || sz > str.length()) {
+            return "";
+        }
+        int numSubStrings = str.length() / sz;
+
+        for (int i = 0; i < numSubStrings; i++) {
+            String sub = str.substring(i * sz, (i + 1) * sz);
+            int sumOfCubes = calculateSumOfCubes(sub);
+            if (sumOfCubes % 2 == 0) {
+                sb.append(reverseString(sub));
+            } else {
+                sb.append(rotateString(sub));
+            }
+        }
+        return sb.toString();
+    }
+
+    // Helper method to reverse a string
+    private static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    // Helper method to rotate a string
+    private static String rotateString(String str) {
+        return str.substring(1) + str.charAt(0);
+    }
+
+    // Helper method to calculate the sum of cubes of digits in a string
+    private static int calculateSumOfCubes(String str) {
+        int sum = 0;
+        for (char c : str.toCharArray()) {
+            int digit = Character.getNumericValue(c);
+            sum += digit * digit * digit;
+        }
+        return sum;
+    }
+
 }
--- src/test/java/com/example/exercismjavakatas/AccumString/BioTest.java
+++ src/test/java/com/example/exercismjavakatas/AccumString/BioTest.java
@@ -16,4 +16,16 @@ public class BioTest {
         Bio b = new Bio();
         assertEquals("GCAU", b.dnaToRna("GCAT"));
     }
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: revRot");
+        testing(Bio.revRot("1234", 0), "");
+        testing(Bio.revRot("", 0), "");
+        testing(Bio.revRot("1234", 5), "");
+        String s = "733049910872815764";
+        testing(Bio.revRot(s, 5), "330479108928157");
+    }
 }
